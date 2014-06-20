df2e1d926a501bed95fdcee2307e650571c9346b
cj <cjlazell@gmail.com>
2014-06-20 15:30:46 -0500

    added debug mode

--- lib/cutest.rb
+++ lib/cutest.rb
@@ -5,7 +5,7 @@ class Cutest
   autoload :Database, 'database'
 
   unless defined?(VERSION)
-    VERSION = "1.3.1"
+    VERSION = "1.3.2"
     FILTER = %r[/(ruby|jruby|rbx)[-/]([0-9\.])+]
     CACHE = Hash.new { |h, k| h[k] = File.readlines(k) }
   end
